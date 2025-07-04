e530c6ce9c6916203cdddd6d8c8de12fa26f0509
jkljggggg <bbbdaru@gmail.com>
2025-07-04 16:40:11 +0530

    Add files via upload

---
+++ Pbxbot/plugins/bot/__init__.py
@@ -0,0 +1,44 @@
+from Pbxbot.core.clients import Pbxbot
+from Pbxbot.core.config import Config, Symbols
+from Pbxbot.core.database import db
+from Pbxbot.plugins.help import BotHelp
+
+
+START_MSG = """
+👋 **𝖦𝗋𝖾𝖾𝗍𝗂𝗇𝗀𝗌, {0} - 𝗐𝖺𝗋𝗋𝗂𝗈𝗋𝗌 𝗈𝖿 RAJA BHAI!** 👹 𝖨 𝖺𝗆 𝗒𝗈𝗎𝗋 𝗍𝗋𝗎𝗌𝗍𝗒 𝖼𝗈𝗆𝗉𝖺𝗇𝗂𝗈𝗇, 𝗍𝗁𝖾 **RAJA BHAI 𝖠𝗌𝗌𝗂𝗌𝗍𝖺𝗇𝗍!** 🚀
+
+👋🏻ɪ ᴀᴍ ᴀᴅᴠᴀɴᴄᴇᴅ ⛏ ᴀɴᴅ sᴜᴘᴇʀғᴀsᴛ ⛓ᴛᴇʟᴇɢʀᴀᴍ  RAJA ᴜsᴇʀʙᴏᴛ 🤖 .
+🦋ᴡʜᴇᴛʜᴇʀ ɪᴛ's ᴄʀᴇᴀᴛɪɴɢ, ᴅᴇʟᴇᴛɪɴɢ 🚫 ᴏʀ ᴜᴘᴅᴀᴛɪɴɢ 🔧 ʏᴏᴜʀ ᴜsᴇʀʙᴏᴛ, ɪ'ᴠᴇ ɢᴏᴛ ʏᴏᴜʀ ʙᴀᴄᴋ 🧹.
+💀ᴍᴀʏ ʏᴏᴜʀ ᴄᴏᴍᴍᴀɴᴅs 📲 ʙᴇ sᴡɪғᴛ ᴀɴᴅ ʏᴏᴜʀ sᴇssɪᴏɴ ʟᴇɢᴇɴᴅᴀʀʏ ☠.
+
+**𝖶𝖾𝗅𝖼𝗈𝗆𝖾 𝗍𝗈 RAJA BHAI 𝖠𝗌𝗌𝗂𝗌𝗍𝖺𝗇𝗍 – 𝗐𝗁𝖾𝗋𝖾 RAJA BHAI 𝗅𝖾𝗀𝖺𝖼𝗒 𝗅𝗂𝗏𝖾𝗌 𝗈𝗇 🤖!**
+
+**❤️ @RAJAmusic67 ❤️**
+"""
+
+HELP_MSG = """
+**⚙️ 𝖧𝖾𝗅𝗉★**
+
+**__» All commands are categorized and you can use these buttons below to navigate each category and get respective commands.__
+__» Feel free to contact us if you need any help regarding the bot.__**
+
+**❤️ @RAJAmusic67 ❤️**
+  
+  **★USERBOT DEPLOY★**
+
+**since i never mentioned how to activate userbot on your account. here's a little guide.
+
+1. deployment is successful and bot is working .
+
+2. send /session to your helper bot.
+
+3. now click on "new" button and Genrate Pyrogram String Session.
+
+4. send /add { Paste Your Genrate Session }
+
+5. follow all the prompts and do the needful.
+
+6. restart the bot with /restart command.**
+
+**❤️ @RAJAmusic67 ❤️**
+"""
